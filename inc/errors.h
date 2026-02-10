8755180cc6edd67bd1b87b7552534307f9dcb76a
sanya-padalko <apadalko90@gmail.com>
2026-02-10 18:57:03 +0300

    Errors
    Добавлены возможные ошибки

---
+++ inc/errors.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define RESET_COLOR  "\033[0m"
+#define RED_COLOR    "\033[31m"
+#define GREEN_COLOR  "\033[32m"
+#define YELLOW_COLOR "\033[33m"
+
+#define line_info __FILE__, __FUNCTION__, __LINE__
+
+#define ERROR_PARSE(type)   printf("ParseBorError called from %s, %s in line %d\n", line_info); \
+                            printf("Error Type %d: ", type); \
+                            ParseBorError(type);
+
+enum BorError_t {
+    OK      =   0,
+    NPTR    =   1,
+    MXDEEP  =   2,
+    WERROR  =   3,
+};
+
+void ParseBorError(BorError_t err);
---
+++ src/errors.cpp
@@ -0,0 +1,19 @@
+#include "errors.h"
+
+void ParseBorError(BorError_t err) {
+    switch (err) {
+        case OK:
+            pritnf(GREEN_COLOR "Everything is ok\n");
+            break;
+        case NPTR:
+            prinntf(RED_COLOR "NULL pointer or accessing a non-existent vertex\n");
+            break;
+        case MXDEEP:
+            printf(RED_COLOR "The word with length more than MAX DEEP\n");
+            break;
+        case WERROR:
+            printf(YELLOW_COLOR "Word isn't in Bor\n");
+            break;
+    }
+    printf(RESET_COLOR);
+}
